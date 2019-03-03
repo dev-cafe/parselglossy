a08cff203891be67ce1ea91ced78b869c6a16fe1
Roberto Di Remigio <roberto.diremigio@gmail.com>
2019-03-03 15:08:10 -0500

    Test flat sections interspersed with keywords
    Signed-off-by: Roberto Di Remigio <roberto.diremigio@gmail.com>

--- tests/test_getkw.py
+++ tests/test_getkw.py
@@ -111,7 +111,7 @@ def test_section(name):
 
 
 @pytest.fixture
-def flat():
+def flat_sections():
     stuff = contents().format(
         PI=math.pi, E=math.e, TAU=2.0 * math.pi, LIST=list(range(1, 5)))
     sects = """topsect {{
@@ -125,11 +125,11 @@ foo<bar> {{
     return sects.format(CONTENTS=stuff)
 
 
-def test_flat_sections(flat):
+def test_flat_sections(flat_sections):
     """Test an input made of two unnested sections, tagged or untagged."""
     ref_dict = {'topsect': dict(reference), 'foo<bar>': dict(reference)}
     grammar = getkw.grammar()
-    tokens = grammar.parseString(flat).asDict()
+    tokens = grammar.parseString(flat_sections).asDict()
 
     assert tokens == ref_dict
     # dump to JSON
@@ -144,7 +144,7 @@ def test_flat_sections(flat):
 
 
 @pytest.fixture
-def nested():
+def nested_sections():
     stuff = contents().format(
         PI=math.pi, E=math.e, TAU=2.0 * math.pi, LIST=list(range(1, 5)))
     sects = """topsect {{
@@ -158,12 +158,12 @@ def nested():
     return sects.format(CONTENTS=stuff)
 
 
-def test_nested_sections(nested):
+def test_nested_sections(nested_sections):
     """Test an input made of two nested sections, tagged or untagged."""
     ref_dict = {'topsect': dict(reference)}
     ref_dict['topsect']['foo<bar>'] = dict(reference)
     grammar = getkw.grammar()
-    tokens = grammar.parseString(nested).asDict()
+    tokens = grammar.parseString(nested_sections).asDict()
 
     assert tokens == ref_dict
     # dump to JSON
@@ -178,6 +178,8 @@ def test_nested_sections(nested):
 
 
 def keywords_and_section(name):
+    stuff = contents().format(
+        PI=math.pi, E=math.e, TAU=2.0 * math.pi, LIST=list(range(1, 5)))
     template = """/* This is a comment */
 int = 42
 // This is another comment
@@ -203,8 +205,6 @@ cmplx_array = [{PI} -2*j, {E}-2.0*J, {TAU}+1.5*i]
 bool_array = [on, true, yes, False, True, false]
 str_array = [foo, bar, "lorem", "IpSuM"]
 """
-    stuff = contents().format(
-        PI=math.pi, E=math.e, TAU=2.0 * math.pi, LIST=list(range(1, 5)))
     inp = template.format(
         PI=math.pi,
         E=math.e,
@@ -225,6 +225,68 @@ def test_keywords_and_section(name):
     ref_dict[name] = dict(reference)
     grammar = getkw.grammar()
     tokens = grammar.parseString(keywords_and_section(name)).asDict()
+
+    assert tokens == ref_dict
+    # dump to JSON
+    getkw_json = StringIO()
+    json.dump(tokens, getkw_json, cls=ComplexEncoder)
+    del tokens
+
+    # load from JSON
+    tokens = json.loads(getkw_json.getvalue(), object_hook=as_complex)
+
+    assert tokens == ref_dict
+
+
+@pytest.fixture
+def keywords_and_flat_sections():
+    stuff = contents().format(
+        PI=math.pi, E=math.e, TAU=2.0 * math.pi, LIST=list(range(1, 5)))
+    template = """/* This is a comment */
+int = 42
+// This is another comment
+dbl = {PI}
+cmplx = -1 -{E}*I
+bool = on
+str = "fooffa"
+
+topsect {{
+    {CONTENTS}
+}}
+
+! Yet another comment
+$raw
+H 0.0 0.0 0.0
+F 1.0 1.0 1.0
+$end
+
+foo<bar> {{
+    {CONTENTS}
+}}
+
+# I love comments!
+int_array = {LIST}
+dbl_array = [{PI}, {E}, {TAU}]
+cmplx_array = [{PI} -2*j, {E}-2.0*J, {TAU}+1.5*i]
+bool_array = [on, true, yes, False, True, false]
+str_array = [foo, bar, "lorem", "IpSuM"]
+"""
+    inp = template.format(
+        PI=math.pi,
+        E=math.e,
+        TAU=2.0 * math.pi,
+        LIST=list(range(1, 5)),
+        CONTENTS=stuff)
+    return inp
+
+
+def test_keywords_and_flat_sections(keywords_and_flat_sections):
+    """Test an input made of keywords and two unnested sections, interspersed."""
+    ref_dict = dict(reference)
+    ref_dict['topsect'] = dict(reference)
+    ref_dict['foo<bar>'] = dict(reference)
+    grammar = getkw.grammar()
+    tokens = grammar.parseString(keywords_and_flat_sections).asDict()
 
     assert tokens == ref_dict
     # dump to JSON
