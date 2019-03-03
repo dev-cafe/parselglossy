514e5d3d292d9866b44c34b918d77023021410e1
Roberto Di Remigio <roberto.diremigio@gmail.com>
2019-03-03 12:42:21 -0500

    Fix parsing of keyword-only inputs
    Signed-off-by: Roberto Di Remigio <roberto.diremigio@gmail.com>

--- tests/test_getkw.py
+++ tests/test_getkw.py
@@ -56,16 +56,15 @@ str_array = [foo, bar, "lorem", "IpSuM"]
 def keywords():
     stuff = contents().format(
         PI=math.pi, E=math.e, TAU=2.0 * math.pi, LIST=list(range(1, 5)))
-    keys = """topsect {{
-    {CONTENTS:s}
-}}
+    keys = """{CONTENTS}
 """
     return keys.format(CONTENTS=stuff)
 
 
 def test_keyword(keywords):
+    """Test an input made only of keywords."""
     grammar = getkw.grammar()
-    tokens = grammar.parseString(keywords).asDict()['topsect']
+    tokens = grammar.parseString(keywords).asDict()
 
     assert tokens == reference
     # dump to JSON
@@ -83,7 +82,7 @@ def section(name):
     stuff = contents().format(
         PI=math.pi, E=math.e, TAU=2.0 * math.pi, LIST=list(range(1, 5)))
     sect = """{NAME} {{
-    {CONTENTS:s}
+    {CONTENTS}
 }}
 """
     return sect.format(NAME=name, CONTENTS=stuff)
@@ -94,6 +93,7 @@ def section(name):
     'foo<bar>',
 ])
 def test_section(name):
+    """Test an input made of one section, tagged or untagged."""
     ref_dict = {name: reference}
     grammar = getkw.grammar()
     tokens = grammar.parseString(section(name)).asDict()
@@ -115,17 +115,18 @@ def flat():
     stuff = contents().format(
         PI=math.pi, E=math.e, TAU=2.0 * math.pi, LIST=list(range(1, 5)))
     sects = """topsect {{
-    {CONTENTS:s}
+    {CONTENTS}
 }}
 
 foo<bar> {{
-    {CONTENTS:s}
+    {CONTENTS}
 }}
 """
     return sects.format(CONTENTS=stuff)
 
 
 def test_flat_sections(flat):
+    """Test an input made of two unnested sections, tagged or untagged."""
     ref_dict = {'topsect': reference, 'foo<bar>': reference}
     grammar = getkw.grammar()
     tokens = grammar.parseString(flat).asDict()
@@ -147,10 +148,10 @@ def nested():
     stuff = contents().format(
         PI=math.pi, E=math.e, TAU=2.0 * math.pi, LIST=list(range(1, 5)))
     sects = """topsect {{
-    {CONTENTS:s}
+    {CONTENTS}
 
     foo<bar> {{
-        {CONTENTS:s}
+        {CONTENTS}
     }}
 }}
 """
@@ -158,10 +159,69 @@ def nested():
 
 
 def test_nested_sections(nested):
+    """Test an input made of two nested sections, tagged or untagged."""
     ref_dict = {'topsect': reference}
     ref_dict['topsect']['foo<bar>'] = dict(reference)
     grammar = getkw.grammar()
     tokens = grammar.parseString(nested).asDict()
+
+    assert tokens == ref_dict
+    # dump to JSON
+    getkw_json = StringIO()
+    json.dump(tokens, getkw_json, cls=ComplexEncoder)
+    del tokens
+
+    # load from JSON
+    tokens = json.loads(getkw_json.getvalue(), object_hook=as_complex)
+
+    assert tokens == ref_dict
+
+
+def keywords_and_section(name):
+    stuff = """/* This is a comment */
+int = 42
+// This is another comment
+dbl = {PI}
+cmplx = -1 -{E}*I
+bool = on
+str = "fooffa"
+
+{NAME} {{
+    {CONTENTS}
+}}
+
+! Yet another comment
+$raw
+H 0.0 0.0 0.0
+F 1.0 1.0 1.0
+$end
+
+# I love comments!
+int_array = {LIST}
+dbl_array = [{PI}, {E}, {TAU}]
+cmplx_array = [{PI} -2*j, {E}-2.0*J, {TAU}+1.5*i]
+bool_array = [on, true, yes, False, True, false]
+str_array = [foo, bar, "lorem", "IpSuM"]
+"""
+    inp = stuff.format(
+        PI=math.pi,
+        E=math.e,
+        TAU=2.0 * math.pi,
+        LIST=list(range(1, 5)),
+        NAME=name,
+        CONTENTS=contents())
+    return inp
+
+
+@pytest.mark.parametrize('name', [
+    'topsect',
+    'foo<bar>',
+])
+def test_keywords_and_section(name):
+    """Test an input made of keywords, one section, tagged or untagged, and more keywords."""
+    ref_dict = dict(reference, **({name: reference}))
+    grammar = getkw.grammar()
+    tokens = grammar.parseString(keywords_and_section(name)).asDict()
 
     assert tokens == ref_dict
     # dump to JSON
