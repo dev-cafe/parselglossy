cf3a4c4bfdc0772af1c4b16657826ebd193ebb5f
Radovan Bast <bast@users.noreply.github.com>
2019-03-10 12:20:35 +0100

    compare the result input dict with reference
    Signed-off-by: Radovan Bast <bast@users.noreply.github.com>

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -33,4 +33,19 @@ def test_validation():
     # now that all keywords have some value, we can check predicates
     check_predicates_node(input_dict, input_dict, template_dict)
 
-#   print(input_dict)
+    reference = {
+        'title': 'this is an example',
+        'scf': {
+            'functional': 'B3LYP',
+            'max_num_iterations': 20,
+            'some_acceleration': True,
+            'thresholds': {
+                 'some_integral_screening': 0.0002,
+                 'energy': 1e-05
+             },
+            'another_number': 10,
+            'some_complex_number': '0.0+0.0j'
+        }
+    }
+
+    assert input_dict == reference
