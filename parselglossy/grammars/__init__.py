eae6b0afd09ba1091f9ab0ff54bc446f0edfe2ff
Roberto Di Remigio <roberto.diremigio@gmail.com>
2019-03-11 11:10:42 -0400

    Tuck available grammars under a subpackage
    Signed-off-by: Roberto Di Remigio <roberto.diremigio@gmail.com>

--- parselglossy/grammars/__init__.py
+++ parselglossy/grammars/__init__.py
@@ -25,39 +25,3 @@
 # For information on the complete list of contributors to the
 # parselglossy library, see: <http://parselglossy.readthedocs.io/>
 #
-
-from parselglossy.validate import type_matches
-import pytest
-
-
-def test_type_matches():
-
-    # str
-    assert type_matches('a string', 'str')
-    assert not type_matches(1.0, 'str')
-
-    # float and int
-    assert type_matches(1.0, 'float')
-    assert type_matches(1.0e-8, 'float')
-    assert type_matches(1, 'int')
-    assert not type_matches(1, 'float')
-
-    # complex
-    assert type_matches(1.0 + 1.0j, 'complex')
-
-    # bool
-    assert type_matches(True, 'bool')
-    assert type_matches(False, 'bool')
-    assert not type_matches(0, 'bool')
-
-    # lists
-    assert type_matches([1, 2, 3], 'List[int]')
-    assert not type_matches((1, 2, 3), 'List[int]')
-    assert not type_matches([1, 2, 3], 'List[float]')
-    assert type_matches([1.0, 2.0, 3.0], 'List[float]')
-    assert not type_matches([1.0, 2.0, 3], 'List[float]')
-
-    # unexpected type input
-    with pytest.raises(ValueError):
-        assert type_matches('example', 'weird')
-        assert type_matches('example', 'List[strange]')
--- tests/test_atoms.py
+++ tests/test_atoms.py
@@ -1,16 +1,43 @@
 #!/usr/bin/env python
+
+#
+# parselglossy -- Generic input parsing library, speaking in tongues
+# Copyright (C) 2019 Roberto Di Remigio, Radovan Bast, and contributors.
+#
+# This file is part of parselglossy.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+# For information on the complete list of contributors to the
+# parselglossy library, see: <http://parselglossy.readthedocs.io/>
+#
+
 # -*- coding: utf-8 -*-
 """Tests for `parselglossy` package."""
 
-import itertools
-import math
 import string
 
 import pytest
 from hypothesis import given
 from hypothesis import strategies as st
 
-from parselglossy import atoms
+from parselglossy.grammars import atoms
 
 
 @given(a=st.sampled_from(atoms.truthy + atoms.falsey))
@@ -33,10 +60,7 @@ def floats(draw):
     understood by the `float_t` atom.
     """
     number = draw(st.floats(allow_nan=False, allow_infinity=False))
-    fmt = draw(
-        st.sampled_from([
-            '{:.20f}', '{:.20e}', '{:.20E}', '{:+.20f}', '{:+.20e}', '{:+.20E}'
-        ]))
+    fmt = draw(st.sampled_from(['{:.20f}', '{:.20e}', '{:.20E}', '{:+.20f}', '{:+.20e}', '{:+.20E}']))
     return fmt.format(number), number
 
 
@@ -63,9 +87,7 @@ def complex_numbers(draw):
     has_real = draw(st.booleans())
     w_space = draw(st.booleans())
     real_fmt = draw(st.sampled_from(['{:.20g}', '{:.20G}']))
-    imag_fmt = draw(
-        st.sampled_from(
-            ['{:+.20g}*j', '{:+.20G}*j', '{:+.20g}*J', '{:+.20G}*J']))
+    imag_fmt = draw(st.sampled_from(['{:+.20g}*j', '{:+.20G}*j', '{:+.20g}*J', '{:+.20G}*J']))
     number_as_string = ''
     if has_real:
         if w_space:
@@ -87,10 +109,9 @@ def test_atoms_complex(a):
 
 
 @given(
-    a=st.sampled_from([
-        ['raw', 'H 0.0 0.0 0.0\nF 1.0 1.0 1.0\n'],
-        ['RAW', 'H 0.0 0.0 0.0\nF 1.0 1.0 1.0\n']
-    ]).map(lambda x: ('${0:s}\n{1:s}$end'.format(x[0], x[1]), x)))
+    a=st.sampled_from([['raw', 'H 0.0 0.0 0.0\nF 1.0 1.0 1.0\n'],
+                       ['RAW',
+                        'H 0.0 0.0 0.0\nF 1.0 1.0 1.0\n']]).map(lambda x: ('${0:s}\n{1:s}$end'.format(x[0], x[1]), x)))
 def test_atoms_data(a):
     tokens = atoms.data_t.parseString(a[0]).asList()
     assert tokens[0] == a[1]
