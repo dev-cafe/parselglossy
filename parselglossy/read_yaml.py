3877b940301e8f1daf148e1e3cb8c204f1a4e9fe
Roberto Di Remigio <roberto.diremigio@gmail.com>
2019-03-12 22:09:52 -0400

    Use open() on Path directly
    Signed-off-by: Roberto Di Remigio <roberto.diremigio@gmail.com>

--- parselglossy/read_yaml.py
+++ parselglossy/read_yaml.py
@@ -36,9 +36,7 @@ from .utils import JSONDict
 def read_yaml_file(file_name: Path) -> JSONDict:
     """Reads a YAML file and returns it as a dictionary.
     """
-    # convert to str() because
-    # pathlib integrates nicely with open only for python >= 3.6
-    with open(str(file_name), 'r') as f:
+    with file_name.open('r') as f:
         try:
             d = yaml.safe_load(f)
         except yaml.YAMLError as e:
