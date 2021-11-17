c7b102a25a114b7a86dc0aa688fdd27cf2f04b1a
Roberto Di Remigio <roberto.diremigio@gmail.com>
2021-11-17 11:10:20 +0100

    Make Black happy

--- parselglossy/generation.py
+++ parselglossy/generation.py
@@ -92,7 +92,9 @@ def cli():
 def get_parse_string_to_dict() -> str:
     with (Path(__file__).parent.absolute() / "grammars/lexer.py").open("r") as buf:
         m = search(
-            r"\# ->->-> SNIP <-<-<-\n(.*)\n\# -<-<-< SNAP >->->-", buf.read(), DOTALL,
+            r"\# ->->-> SNIP <-<-<-\n(.*)\n\# -<-<-< SNAP >->->-",
+            buf.read(),
+            DOTALL,
         )
     if m is not None:
         return f"""\n
