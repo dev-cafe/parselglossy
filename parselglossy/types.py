8b00da13ec38900d470b7eb08bbcdce5013cb142
Roberto Di Remigio <robertodr@users.noreply.github.com>
2022-07-18 09:38:30 +0200

    Fix RTD build (#114)

--- parselglossy/types.py
+++ parselglossy/types.py
@@ -53,13 +53,12 @@ def type_matches(value: AllowedTypes, expected_type: str) -> Optional[bool]:
     Parameters
     ----------
     value : AllowedTypes
-        Value whose type needs to be checked
+      Value whose type needs to be checked
     expected_type : str
 
     Notes
     -----
-    Allowed types T are: `str`, `int`, `float`, `complex`, `bool`,
-    as well as `List[T]`.
+    Allowed types T are: `str`, `int`, `float`, `complex`, `bool`, as well as `List[T]`.
 
     Returns
     -------
@@ -68,18 +67,18 @@ def type_matches(value: AllowedTypes, expected_type: str) -> Optional[bool]:
     Raises
     ------
     ValueError
-        If expected_type is not among the allowed types.
+      If expected_type is not among the allowed types.
 
     Notes
     -----
     Complex numbers are a tad more fastidious, as they *might be* read in as
     strings. To avoid false negatives, we have a nasty hack, for which RDR will
     forever burn in hell. For complex and List[complex] we will re-run type
-    checking after type
-    casting if and only if:
-      - We expected complex and List[complex].
-      - We got a str or List[str].
-      - The string are in the right format for the type casting operator.
+    checking after type casting if and only if:
+
+    - We expected complex and List[complex].
+    - We got a str or List[str].
+    - The string are in the right format for the type casting operator.
     """
 
     # first verify whether expected_type is allowed
